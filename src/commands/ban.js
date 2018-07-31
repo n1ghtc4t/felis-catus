6661422f2048f214fc28a785f0dcdde5b9ab1d45
radix <onememeonly@gmail.com>
2018-07-31 04:10:28 -0500

    im gay

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -4,18 +4,29 @@ module.exports = {
   help: {
     name: 'ban',
     aliases: ['b'],
-    args: '[user] <messages_to_delete> <reason>',
-    desc: 'Bans a user!'
+    args: '[user] <reason>',
+    desc: 'bans a user!'
   },
   async execute(args, client, msg) {
-    if(!args[0])
-      return msg.channel.send('⚠ | No user specified');
+    let user = msg.mentions.members.first() || msg.guild.members.get(msg.mentions.members.first().id);
+    let reason = args.slice(3).join(/ +/);
 
-    args[0].ban(args[1] ? args[1] : 0, args[2] ? args[2] : `[${msg.author.tag}] No reason specified`)
+    if(!user || msg.mentions.members.size > 1)
+      return msg.channel.send('⚠ | No user specified or too many users were provided');
+
+    // Deleting messages soon:tm:
+    // if(typeof toDelete !== number)
+    //   return msg.channel.send('⚠ | Amount of messages to delete is not a number');
+
+    user.ban(reason ? reason : `[${msg.author.tag}] No reason specified`)
       .then(() => msg.channel.send(`🔨 | ${args[0].user.username} successfully banned.`))
-      .catch(e => msg.channel.send('⚠ | An error occurred whilst executing the command.')
-
-      client.users.get('260246864979296256')
-        .send(`⚠ | An error has occurred, details below:\n\nUser: ${msg.author.tag}\nCommand executed: ${msg.content.slice(5)}\n\nStacktrace:\n${e.stack}`));
+      .catch(e => {
+        msg.channel.send('⚠ | An error occurred whilst executing the command.')
+                  client.users.get('260246864979296256')
+                    .send(`⚠ | An error has occurred, details below:\n\n
+                      User: ${msg.author.tag}\n
+                      Command executed: ${msg.content.slice(5)}\n\n
+                      Stacktrace:\n${e.stack}`);
+      });
   }
 }
